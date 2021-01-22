7be32d96c31c356c5fb2f4bc671510378953b6a5
Samuel Lee <samuelxlee@gmail.com>
2021-01-21 20:08:58 -0800

    wrote the model and router for the tasks endpoint

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,17 @@
-// build your `Task` model here
+const db = require("../../data/dbConfig");
+
+module.exports = {
+    getTasks,
+    insertTask
+}
+
+function getTasks() {
+    return db('tasks');
+}
+
+function insertTask(task) {
+    return db('tasks').insert(task)
+        .then(([id]) => {
+            return db('tasks').where('task_id', id).first();
+        });
+}
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,31 @@
-// build your `/api/tasks` router here
+const express = require("express");
+
+const Tasks = require("./model");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    Tasks.getTasks()
+        .then(tasks => {
+            res.status(200).json(tasks);
+        })
+        .catch(err => {
+            res.status(500).json({ message: "Failed to get tasks" });
+        });
+});
+
+router.post("/", (req, res) => {
+    Tasks.insertTask(req.body)
+        .then(task => {
+            if (!task.project_id || !task.task_description) {
+                res.status(400).json({ message: "Please include a description and project id" });
+            } else {
+                res.status(201).json(task);
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: "Failed to insert task" });
+        })
+});
+
+module.exports = router;
