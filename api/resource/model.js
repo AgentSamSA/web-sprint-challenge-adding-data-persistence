f7479f3991d0cc0b81d3ba7822c498fd74aedda1
Samuel Lee <samuelxlee@gmail.com>
2021-01-21 20:04:16 -0800

    wrote the model and router for the resources endpoint

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,17 @@
-// build your `Resource` model here
+const db = require("../../data/dbConfig");
+
+module.exports = {
+    getResources,
+    insertResource
+}
+
+function getResources() {
+    return db('resources');
+}
+
+function insertResource(resource) {
+    return db('resources').insert(resource)
+        .then(([id]) => {
+            return db('resources').where('resource_id', id).first();
+        });
+}
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,27 @@
-// build your `/api/resources` router here
+const express = require("express");
+
+const Resources = require("./model");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    Resources.getResources()
+        .then(resources => {
+            res.status(200).json(resources);
+        })
+        .catch(err => {
+            res.status(500).json({ message: "Failed to get resources" });
+        });
+});
+
+router.post("/", (req, res) => {
+    Resources.insertResource(req.body)
+        .then(resource => {
+            res.status(201).json(resource);
+        })
+        .catch(err => {
+            res.status(500).json({ message: "Failed to insert resource" });
+        })
+});
+
+module.exports = router;
