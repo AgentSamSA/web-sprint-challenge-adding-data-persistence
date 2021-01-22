ca408e18104460444e7ddd596cedff39c7a83e6c
Samuel Lee <samuelxlee@gmail.com>
2021-01-21 20:00:59 -0800

    wrote the model and router for the projects endpoint

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,27 @@
-// build your `/api/projects` router here
+const express = require("express");
+
+const Projects = require("./model");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    Projects.getProjects()
+        .then(projects => {
+            res.status(200).json(projects);
+        })
+        .catch(err => {
+            res.status(500).json({ message: "Failed to get projects" });
+        });
+});
+
+router.post("/", (req, res) => {
+    Projects.insertProject(req.body)
+        .then(project => {
+            res.status(201).json(project);
+        })
+        .catch(err => {
+            res.status(500).json({ message: "Failed to insert project" });
+        })
+});
+
+module.exports = router;
